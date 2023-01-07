8f108febc8296ec1cad596dfc80700b82b6086ba
Laurin Birchler <58566423+laurinbirchler@users.noreply.github.com>
2023-01-07 17:41:38 +0100

    added documentation

--- src/main/java/com/github/laurinbirchler/algorithms/sort/StoogeSort.java
+++ src/main/java/com/github/laurinbirchler/algorithms/sort/StoogeSort.java
@@ -1,13 +1,26 @@
 package com.github.laurinbirchler.algorithms.sort;
 
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.DisplayName;
 
-@DisplayName("Testing Stooge Sort")
+/**
+ * Stooge sort is a sorting algorithm that is a recursive, inefficient sorting algorithm with a time complexity
+ * of O(n^(log 3/log 1.5)). It is based on the idea of sorting elements in a specific order by comparing and
+ * swapping elements that are not in that order. It gets its name from the "Three Stooges" because it resembles
+ * the characters' slapstick antics.
+ * <p>
+ * The algorithm has a very poor running time compared to other sorting algorithms. Since even bubble sort has a better
+ * running time, stooge sort is only of interest for demonstration purposes.
+ *
+ * @author Laurin Birchler
+ * @version 1.0
+ * @see <a href="https://en.wikipedia.org/wiki/Stooge_Sort">Stooge Sort (Wikipedia)</a>
+ * @see <a href="https://www.geeksforgeeks.org/stooge-sort/">Stooge Sort (GeeksForGeeks)</a>
+ * @since 1.0
+ */
 public class StoogeSort implements SortingAlgorithm {
     @Override
     public <T extends Comparable<T>> T[] sort(T @NotNull [] arr) {
-        sort(arr, 0, arr.length );
+        sort(arr, 0, arr.length);
         return arr;
     }
 
@@ -24,7 +37,7 @@ public class StoogeSort implements SortingAlgorithm {
      * @param l the left index of the subarray to be sorted
      * @param r the right index of the subarray to be sorted
      */
-    private void sort(int[] arr, int l, int r) {
+    private void sort(int @NotNull [] arr, int l, int r) {
 
         // If first element is smaller than last, swap them
         if (arr[r - 1] < arr[l]) {
@@ -33,6 +46,7 @@ public class StoogeSort implements SortingAlgorithm {
             arr[r - 1] = temp;
         }
 
+        // TODO: This is duplicated code. Can we do better?
         int n = r - l;
 
         // If there are 2 or more elements in the array then sort the first 2/3 elements and last 2/3 elements recursively
@@ -55,7 +69,7 @@ public class StoogeSort implements SortingAlgorithm {
      * @param r the right index of the subarray to be sorted
      * @param <T> the type of elements in the array
      */
-    private <T extends Comparable<T>> void sort(T[] arr, int l, int r) {
+    private <T extends Comparable<T>> void sort(T @NotNull [] arr, int l, int r) {
 
         // If first element is smaller than last, swap them
         if (arr[r - 1].compareTo(arr[l]) < 0) {
@@ -64,6 +78,7 @@ public class StoogeSort implements SortingAlgorithm {
             arr[r - 1] = temp;
         }
 
+        // TODO: This is duplicated code. Can we do better?
         int n = r - l;
 
         // If more than 2 elements are present in the array then recursively sort first 2/3 elements and last 2/3 elements
